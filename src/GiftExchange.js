3ed4c6f7b14524261800dc4c793f3454bbb6d43b
Jonathan Davidson <jdavidson@netsplasher.com>
2020-11-24 19:58:11 -0600

    Add logResults method

--- src/GiftExchange.js
+++ src/GiftExchange.js
@@ -1,14 +1,50 @@
 const { ModuleKind, ModuleResolutionKind } = require("typescript");
 
 class GiftExchange {
+
+  people = [
+    {
+      id: 1,
+      name: 'Jonathan'
+    },
+    {
+      id: 2,
+      name: 'Erica'
+    }
+  ];
+
+  restrictions = [];
+
+  results = [
+    {
+      giverId: 1,
+      recipientId: 2
+    },
+    {
+      giverId: 2,
+      recipientId: 1
+    }
+  ];
+
   constructor(config) {
-    this.people = config.people;
-    this.restrictions = config.restrictions;
+    // this.people = config.people;
+    // this.restrictions = config.restrictions;
+    this.pickNames();
+  }
+
+  pickNames() {
+    console.log('picking names');
   }
 
   logResults() {
     console.log('logging results');
+    this.results.forEach((result) => {
+      const giver = this.people.find(person => result.giverId === person.id);
+      const recipient = this.people.find(person => result.recipientId === person.id);
+      console.log(`${giver.name} buys for ${recipient.name}`);
+    });
   }
+
 };
 
 module.exports = GiftExchange;
